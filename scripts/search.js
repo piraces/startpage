e01079f4f38a571be2fc75e631093ebedbd26c7b
piraces <raul.piraces@gmail.com>
2021-05-04 21:40:33 +0200

    Better encoding of query

--- scripts/search.js
+++ scripts/search.js
@@ -2,7 +2,7 @@ function search(e) {
     if (e.code === "Enter") {
         var searchBar = document.getElementById("search-bar");
         var queryString = searchBar.value;
-        var queryStringEncoded = encodeURI(queryString);
+        var queryStringEncoded = encodeURIComponent(queryString);
         window.open('https://duckduckgo.com/?q=' + queryStringEncoded, '_self');
     }
 }

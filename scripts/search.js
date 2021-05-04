b6e111d96a0390fe58e4e99244c1065da0632ade
piraces <raul.piraces@gmail.com>
2021-05-04 21:37:47 +0200

    Fix some problems

--- scripts/search.js
+++ scripts/search.js
@@ -2,7 +2,7 @@ function search(e) {
     if (e.code === "Enter") {
         var searchBar = document.getElementById("search-bar");
         var queryString = searchBar.value;
-        searchBar.value = '';
-        window.open('https://duckduckgo.com/?q=' + queryString, '_self');
+        var queryStringEncoded = encodeURI(queryString);
+        window.open('https://duckduckgo.com/?q=' + queryStringEncoded, '_self');
     }
 }
